9593b58bf71ec0408b48454e56ad83eab61c16a2
Charley Lewittes <ctlewitt@gmail.com>
2016-07-28 18:50:13 -0400

    added ability to store and reload user-created functions, using pickle

--- concatenative_language/concat_compiler.py
+++ concatenative_language/concat_compiler.py
@@ -4,7 +4,8 @@ from concatenative_language.function import Function
 from concatenative_language.utils import append_with_num_type_cast
 from concatenative_language.calculator import add, sub, mul, div, clr, prt
 from concatenative_language.stack_operations import dup, drop, swap
-
+from concatenative_language.compilation_functions import enter_compile_mode, exit_compile_mode
+import pickle
 
 class ConcatCompliler:
     def __init__(self):
@@ -12,45 +13,53 @@ class ConcatCompliler:
         self.compile_mode = False
         self.compile_instruction_list = []
         self.compile_function_name = ""
-        self.functions = {
-            ":": Function.callback(self.enter_compile_mode, True),
-            ";": Function.callback(self.exit_compile_mode, True),
-            "+": Function.callback(add),
-            "-": Function.callback(sub),
-            "*": Function.callback(mul),
-            "/": Function.callback(div),
-            "clr": Function.callback(clr),
-            "print": Function.callback(prt),
-            "dup": Function.callback(dup),
-            "drop": Function.callback(drop),
-            "swap": Function.callback(swap),
-            "sq": Function.instructions(["dup", "*"]),
-            "fourth": Function.instructions(["sq", "sq"]),
-            "add2and2": Function.instructions([2, 2, "+"])
-        }
+        self.functions = None
+        self.load_functions()
 
-             # self.function_interpreter = Function()
+    def load_functions(self):
+        try:
+            with open("saved_functions.pickle", "rb") as func_file:
+                self.functions = pickle.load(func_file)
+        except IOError:
+            self.functions = {
+                ":": Function.callback(enter_compile_mode, True),
+                ";": Function.callback(exit_compile_mode, True),
+                "+": Function.callback(add),
+                "-": Function.callback(sub),
+                "*": Function.callback(mul),
+                "/": Function.callback(div),
+                "clr": Function.callback(clr),
+                "print": Function.callback(prt),
+                "dup": Function.callback(dup),
+                "drop": Function.callback(drop),
+                "swap": Function.callback(swap),
+                "sq": Function.instructions(["dup", "*"]),
+                "fourth": Function.instructions(["sq", "sq"]),
+                "add2and2": Function.instructions([2, 2, "+"])
+            }
 
-    def enter_compile_mode(self, *args):
-        self.compile_mode = True
-        self.compile_instruction_list = []
-        self.compile_function_name = ""
 
+#     def enter_compile_mode(self, *args):
+#         print(self)
+#         print("Entering compile mode")
+#         self.compile_mode = True
+# #        print(self.compile_mode)
+#         self.compile_instruction_list = []
+#         self.compile_function_name = ""
+#
+#     def exit_compile_mode(self, *args):
+#         self.compile_mode = False
+#         self.functions[self.compile_function_name] = Function.instructions(self.compile_instruction_list)
+#         self.compile_function_name = ""
+#         self.compile_instruction_list = []
+#         # also want to finish saving function
 
-    def exit_compile_mode(self, *args):
-        self.compile_mode = False
-        self.functions[self.compile_function_name] = Function.instructions(self.compile_instruction_list)
-        self.compile_function_name = ""
-        self.compile_instruction_list = []
-        # also want to finish saving function
-
-    #while True:
+    # while True:
         # line = sys.stdin.read()
     def interpret_file(self):
         for line in fileinput.input():
             for token in line.split():
                 if self.compile_mode:
-                    print("in compile mode")
                     # if token is immediate function, execute without compiling
                     if self.functions.get(token) is not None and self.functions[token].immediate:
                         self.execute(self.functions[token])
@@ -66,20 +75,21 @@ class ConcatCompliler:
                             self.compile_function_name = token
                         else:
                             append_with_num_type_cast(self.compile_instruction_list, token)
-                            print(self.compile_function_name + str(self.compile_instruction_list))
                 else:
-                    print("not in compile mode")
                     if token in self.functions:
                         self.execute(self.functions[token])
                     elif token.isdigit():  # need better check than this (for floats)
                         self.stack.append(int(token))  # need to convert into number
-
+        # for function in self.functions:
+        #     print(function)
+        with open("saved_functions.pickle", "wb") as func_file:
+            pickle.dump(self.functions, func_file, pickle.HIGHEST_PROTOCOL)
 
     # execute a function either as a callback or list of instructions
     def execute(self, function):
         # callback function--just execute
         if function.built_in:
-            function.function(self.stack) # call function's function and execute stack
+            function.function(self) # call function's function and execute stack
         # function with list of instructions--interpret each
         else:
             self.interpret(function)
@@ -93,22 +103,8 @@ class ConcatCompliler:
                 self.execute(self.functions[word])
 
 
-
-
 my_compiler = ConcatCompliler()
 my_compiler.interpret_file()
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # introspection
