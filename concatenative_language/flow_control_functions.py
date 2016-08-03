11693953b07ffcfd6bfcad6a7f2616aa4199e1f9
Charley Lewittes <ctlewitt@gmail.com>
2016-08-03 18:51:02 -0400

    fixed but in if function

--- concatenative_language/flow_control_functions.py
+++ concatenative_language/flow_control_functions.py
@@ -1,10 +1,12 @@
+# want to do while also
+
+
 def if_conditional(compiler):
-    false_outcome = compiler.stack().pop()
-    true_outcome = compiler.stack().pop()
-    condition = compiler.stack().pop()
+    false_outcome = compiler.stack.pop()
+    true_outcome = compiler.stack.pop()
+    condition = compiler.stack.pop()
     if condition:
         compiler.execute(true_outcome)
     else:
         compiler.execute(false_outcome)
 
-#want to do while also
