072c4447f31c8447276d40b6cee7e95451525117
Charley Lewittes <ctlewitt@gmail.com>
2016-08-03 09:58:10 -0400

    adding casting to support other variable types besides numbers e.g., strings and booleans

--- concatenative_language/utils.py
+++ concatenative_language/utils.py
@@ -15,14 +15,36 @@ def is_numeric(num):
 
 def cast_to_number_if_possible(word):
     try:
-        return int(word)
+        return True, int(word)
     except ValueError:
         try:
-            return float(word)
+            return True, float(word)
         except ValueError:
-            return word
+            return False, word
 
 
-def append_with_num_type_cast(the_list, token):
+def cast_to_bool_if_possible(word):
+    try:
+        return True, bool(word)
+    except ValueError:
+        return False, word
+
+
+def cast_to_str_if_possible(word):
+    if len(word) >=2 and ((word[0] == "'" and word[-1] == "'") or (word[0] == '"' and word[-1] == '"')):
+        return True, word[1:len(word)-1]
+    else:
+        return False, word
+
+
+def append_with_type_cast(the_list, token):
+    is_num, num = cast_to_number_if_possible(token)
+    if is_num:
+        the_list.append(num)
+        return
+    is_bool, bool_val = cast_to_bool_if_possible(token)
+    if is_bool:
+        the_list.append(bool_val)
+
     the_list.append(cast_to_number_if_possible(token))
 
