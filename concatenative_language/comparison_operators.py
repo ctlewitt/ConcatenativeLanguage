4cc97af53de931b751ff41194a5020471756fb98
Charley Lewittes <ctlewitt@gmail.com>
2016-08-03 18:23:53 -0400

    created stubs for comparison functions

---
+++ concatenative_language/comparison_operators.py
@@ -0,0 +1,25 @@
+
+def less(compiler):
+    pass
+
+
+def greater(compiler):
+    pass
+
+
+def less_or_equal(compiler):
+    pass
+
+
+def greater_or_equal(compiler):
+    pass
+
+
+def equal(compiler):
+    pass
+
+
+def not_equal(compiler):
+    pass
+
+
