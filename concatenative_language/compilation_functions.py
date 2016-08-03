c1a39cf9bf8506c28e8849b772c02f7443b35a61
Charley Lewittes <ctlewitt@gmail.com>
2016-08-03 18:10:45 -0400

    rewrote compilation functions to handle changing into and out of copmile and block mode and to save functions to the name space or stack as appropriate

--- concatenative_language/compilation_functions.py
+++ concatenative_language/compilation_functions.py
@@ -2,20 +2,29 @@ from concatenative_language.function import Function
 
 
 def enter_compile_mode(compiler):
-    print("in : function")
+    print("in define function")
     print("Entering compile mode")
     compiler.compile_mode = True
-#        print(self.compile_mode)
-    compiler.compile_instruction_list = []
     compiler.compile_function_name = ""
+
 
 def enter_block_mode(compiler):
-    pass
-
-def exit_compile_and_block_mode(compiler):
-    print("in ; function")
-    compiler.compile_mode = False
-    compiler.functions[compiler.compile_function_name] = Function.instructions(compiler.compile_instruction_list)
-    compiler.compile_function_name = ""
+    print("in { function")
+    compiler.block_mode = True
     compiler.compile_instruction_list = []
 
+
+def exit_compile_and_block_mode(compiler):
+    print("in } function")
+    # in compile_mode (and block_mode), so creating named function
+    if compiler.compile_mode:
+        compiler.functions[compiler.compile_function_name] = Function.instructions(compiler.compile_instruction_list)
+    # just in block_mode, so creating anonymous funtion to be pushed onto stack
+    else:
+        compiler.stack.append(Function.instructions(compiler.compile_instruction_list))
+    # reset compiler's function name and instruction list attributes
+    compiler.compile_function_name = ""
+    compiler.compile_instruction_list = []
+    # reset compile mode and block mode to false to exit modes and re-enter execution mode (implicitly)
+    compiler.compile_mode = False
+    compiler.block_mode = False
