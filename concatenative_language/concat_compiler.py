633563fd1562da2608a3a1ceb509580780edbeda
Charley Lewittes <ctlewitt@gmail.com>
2016-08-04 16:39:33 -0400

    handle nested function definitions

--- concatenative_language/concat_compiler.py
+++ concatenative_language/concat_compiler.py
@@ -1,11 +1,11 @@
 import fileinput
 import sys
 from concatenative_language.function import Function
-from concatenative_language.utils import append_with_type_cast
+from concatenative_language.utils import append_with_type_cast, print_stack
 from concatenative_language.calculator import add, sub, mul, div, clr, prt
-from concatenative_language.stack_operations import dup, drop, swap
+from concatenative_language.stack_operations import dup, drop, swap, rot, dip
 from concatenative_language.compilation_functions import enter_compile_mode, exit_compile_and_block_mode, enter_block_mode
-from concatenative_language.flow_control_functions import if_conditional, while_loop
+from concatenative_language.flow_control_functions import if_conditional, while_loop, do
 from concatenative_language.comparison_operators import less, less_or_equal, greater, greater_or_equal, equal, not_equal
 import pickle
 import re
@@ -31,6 +31,7 @@ class ConcatCompliler:
                 "define": Function.callback(enter_compile_mode, True),
                 "{": Function.callback(enter_block_mode, True),
                 "}": Function.callback(exit_compile_and_block_mode, True),
+                "do": Function.callback(do),
                 "if": Function.callback(if_conditional),
                 "while": Function.callback(while_loop),
                 "<": Function.callback(less),
@@ -48,9 +49,12 @@ class ConcatCompliler:
                 "dup": Function.callback(dup),
                 "drop": Function.callback(drop),
                 "swap": Function.callback(swap),
+                "dip": Function.callback(dip),
+                "rot": Function.callback(rot),
                 "sq": Function.instructions(["dup", "*"]),
                 "fourth": Function.instructions(["sq", "sq"]),
-                "add2and2": Function.instructions([2, 2, "+"])
+                "add2and2": Function.instructions([2, 2, "+"]),
+                "show_stack": Function.callback(print_stack)
             }
 
 
@@ -94,11 +98,31 @@ class ConcatCompliler:
     # go through each instruction in a list of instructions function
     def interpret(self, function):
         for word in function.function:
-            if word in self.functions:
+            if self.functions.get(word) is not None and self.functions[word].immediate:
+                self.execute(self.functions[word])
+            # if in compile mode but not block, function needs name
+            elif self.compile_mode and not self.block_mode:
+                # Once in compilation mode, var/function name must be next token
+                # might need to allow for reassigning functions (maybe if not built in, allow reassignment)...
+                if word in self.functions:
+                    raise Exception("cannot redeclare function")
+                self.compile_function_name = word
+            # in block mode (could be in compile mode or not; doesn't matter) get each command in function
+            elif self.block_mode:
+                append_with_type_cast(self.compile_instruction_list, word, self.functions)
+            # executing (ie, not compile or block mode)
+            #if word in self.functions:
+
+            elif word in self.functions:
+                print(word)
                 self.execute(self.functions[word])
             else:
                 self.stack.append(word)
+                self.execute(self.functions['show_stack'])
 
+
+# might want to pull out large swath of duplicate code in interpret function (duplicated from interpret file)
+    # def handle_compilation
 
 my_compiler = ConcatCompliler()
 my_compiler.interpret_file()
