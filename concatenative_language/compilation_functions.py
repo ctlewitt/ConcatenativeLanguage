d2e15ae4ebfed3565bd010f5448c722dd0ded9ca
Charley Lewittes <ctlewitt@gmail.com>
2016-08-04 15:11:55 -0400

    fixed bug: compile nested functions when seen during a function's execution

--- concatenative_language/compilation_functions.py
+++ concatenative_language/compilation_functions.py
@@ -1,26 +1,40 @@
 from concatenative_language.function import Function
 
 
+# define:
 def enter_compile_mode(compiler):
     compiler.compile_mode = True
     compiler.compile_function_name = ""
 
 
+# { :
 def enter_block_mode(compiler):
-    compiler.block_mode = True
-    compiler.compile_instruction_list = []
+    # print("block depth: {}".format(compiler.block_depth))
+    if compiler.block_depth == 0:
+        compiler.block_mode = True
+        compiler.compile_instruction_list = []
+    else:
+        compiler.compile_instruction_list.append("{")
+    compiler.block_depth += 1
 
 
+
+# } :
 def exit_compile_and_block_mode(compiler):
     # in compile_mode (and block_mode), so creating named function
-    if compiler.compile_mode:
-        compiler.functions[compiler.compile_function_name] = Function.instructions(compiler.compile_instruction_list)
-    # just in block_mode, so creating anonymous funtion to be pushed onto stack
+    compiler.block_depth -= 1
+    if compiler.block_depth == 0:
+        # print("saving function: {}".format(compiler.compile_instruction_list))
+        if compiler.compile_mode:
+            compiler.functions[compiler.compile_function_name] = Function.instructions(compiler.compile_instruction_list)
+        # just in block_mode, so creating anonymous funtion to be pushed onto stack
+        else:
+            compiler.stack.append(Function.instructions(compiler.compile_instruction_list))
+        # reset compiler's function name and instruction list attributes
+        compiler.compile_function_name = ""
+        compiler.compile_instruction_list = []
+        # reset compile mode and block mode to false to exit modes and re-enter execution mode (implicitly)
+        compiler.compile_mode = False
+        compiler.block_mode = False
     else:
-        compiler.stack.append(Function.instructions(compiler.compile_instruction_list))
-    # reset compiler's function name and instruction list attributes
-    compiler.compile_function_name = ""
-    compiler.compile_instruction_list = []
-    # reset compile mode and block mode to false to exit modes and re-enter execution mode (implicitly)
-    compiler.compile_mode = False
-    compiler.block_mode = False
+        compiler.compile_instruction_list.append("}")
