5cfaa1ce36e4c74823eae1e995a4dc27da7680d1
Charley Lewittes <ctlewitt@gmail.com>
2016-08-04 09:32:17 -0400

    wrote while function and added comments and documentation for flow control functions

--- concatenative_language/flow_control_functions.py
+++ concatenative_language/flow_control_functions.py
@@ -1,12 +1,33 @@
-# want to do while also
+# do for loop
 
-
+# if :takes 3 parameters: condition, true outcome, and false outcome; 0 return values
+# evaluates condition and executes true/false function based on true/false outcome
 def if_conditional(compiler):
+    # get parameters
     false_outcome = compiler.stack.pop()
     true_outcome = compiler.stack.pop()
     condition = compiler.stack.pop()
+    # if true, do true outcome
     if condition:
         compiler.execute(true_outcome)
+    # if false, do false outcome
     else:
         compiler.execute(false_outcome)
 
+
+# while :takes 2 parameters: condition function and block; 0 return values
+# recursively executes while loop's block until condition evaluates to false
+def while_loop(compiler):
+    # get parameters
+    block = compiler.stack.pop()
+    condition_func = compiler.stack.pop()
+    # evaluate condition function
+    compiler.execute(condition_func)
+    condition = compiler.stack.pop()
+    # while condition is true, recursively repeat
+    if condition:
+        compiler.execute(block)
+        # leave parameters on stack for next execution
+        compiler.stack.append(condition_func)
+        compiler.stack.append(block)
+    # if condition is false, do nothing
