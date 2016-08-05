40ed6feb1a46d1ff04a5f5370f6356e12f6b553d
Charley Lewittes <ctlewitt@gmail.com>
2016-08-05 16:25:31 -0400

    changed 'compiler' to 'interpreter' to fit nature of program

--- concatenative_language/flow_control_functions.py
+++ concatenative_language/flow_control_functions.py
@@ -2,39 +2,39 @@
 
 # if :takes 3 parameters: condition, true outcome, and false outcome; 0 return values
 # evaluates condition and executes true/false function based on true/false outcome
-def if_conditional(compiler):
+def if_conditional(interpreter):
     # get parameters
-    false_outcome = compiler.stack.pop()
-    true_outcome = compiler.stack.pop()
-    condition_func = compiler.stack.pop()
-    compiler.execute(condition_func)
-    condition = compiler.stack.pop()
+    false_outcome = interpreter.stack.pop()
+    true_outcome = interpreter.stack.pop()
+    condition_func = interpreter.stack.pop()
+    interpreter.execute(condition_func)
+    condition = interpreter.stack.pop()
     # if true, do true outcome
     if condition:
-        compiler.execute(true_outcome)
+        interpreter.execute(true_outcome)
     # if false, do false outcome
     else:
-        compiler.execute(false_outcome)
+        interpreter.execute(false_outcome)
 
 
 # while :takes 2 parameters: condition function and block; 0 return values
 # recursively executes while loop's block until condition evaluates to false
-def while_loop(compiler):
+def while_loop(interpreter):
     # get parameters
-    block = compiler.stack.pop()
-    condition_func = compiler.stack.pop()
+    block = interpreter.stack.pop()
+    condition_func = interpreter.stack.pop()
     # evaluate condition function
-    compiler.execute(condition_func)
-    condition = compiler.stack.pop()
+    interpreter.execute(condition_func)
+    condition = interpreter.stack.pop()
     # while condition is true, recursively repeat
     if condition:
-        compiler.execute(block)
+        interpreter.execute(block)
         # leave parameters on stack for next execution
-        compiler.stack.append(condition_func)
-        compiler.stack.append(block)
-        while_loop(compiler)
+        interpreter.stack.append(condition_func)
+        interpreter.stack.append(block)
+        while_loop(interpreter)
     # if condition is false, do nothing
 
 
-def do(compiler):
-    compiler.execute(compiler.stack.pop())
+def do(interpreter):
+    interpreter.execute(interpreter.stack.pop())
