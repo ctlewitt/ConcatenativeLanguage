da68c59a32c93ab86726c282e31c30df96f37473
Charley Lewittes <ctlewitt@gmail.com>
2016-08-08 17:02:07 -0400

    changed comparison functions (e.g., <, ==, etc) to burn both the left and right parameters and just return True or False

--- concatenative_language/comparison_operators.py
+++ concatenative_language/comparison_operators.py
@@ -1,34 +1,35 @@
-# < :removes top element from stack, compares with second to last and pushes boolean result of comparison onto stack
+# < :removes top 2 elements from stack, compares, and pushes boolean result of comparison onto stack
 def less(compiler):
     right, left = get_right_left(compiler)
     compiler.stack.append(left < right)
 
 
-# > :removes top element from stack, compares with second to last and pushes boolean result of comparison onto stack
+# > :removes top 2 elements from stack, compares, and pushes boolean result of comparison onto stack
 def greater(compiler):
     right, left = get_right_left(compiler)
     compiler.stack.append(left > right)
 
 
-# <= :removes top element from stack, compares with second to last and pushes boolean result of comparison onto stack
+# <= :removes top 2 elements from stack, compares, and pushes boolean result of comparison onto stack
 def less_or_equal(compiler):
     right, left = get_right_left(compiler)
     compiler.stack.append(left <= right)
 
 
-# >= :removes top element from stack, compares with second to last and pushes boolean result of comparison onto stack
+# >= :removes top 2 elements from stack, compares, and pushes boolean result of comparison onto stack
 def greater_or_equal(compiler):
     right, left = get_right_left(compiler)
     compiler.stack.append(left >= right)
 
 
-# == :removes top element from stack, compares with second to last and pushes boolean result of comparison onto stack
+# == :removes top 2 elements from stack, compares, and pushes boolean result of comparison onto stack
 def equal(compiler):
     right, left = get_right_left(compiler)
+    print("ans to ==: {}".format(left == right))
     compiler.stack.append(left == right)
 
 
-# != :removes top element from stack, compares with second to last and pushes boolean result of comparison onto stack
+# != :removes top 2 elements from stack, compares, and pushes boolean result of comparison onto stack
 def not_equal(compiler):
     right, left = get_right_left(compiler)
     compiler.stack.append(left != right)
@@ -36,4 +37,4 @@ def not_equal(compiler):
 
 # helper: returns top and next value from stack as right and left values respectively for comparison
 def get_right_left(compiler):
-    return compiler.stack.pop(), compiler.stack[-1]
+    return compiler.stack.pop(), compiler.stack.pop()
