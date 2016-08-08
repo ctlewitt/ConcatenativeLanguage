cc6d402f31d5c94e8d704483d9d61a8af8cd658a
Charley Lewittes <ctlewitt@gmail.com>
2016-08-08 19:02:42 -0400

    added tests for and() and or() functions

---
+++ concatenative_language_test/test_boolean_operators.py
@@ -0,0 +1,43 @@
+import unittest
+from concatenative_language.concat_interpreter import ConcatInterpreter
+
+
+class TestBooleanFunctions(unittest.TestCase):
+    def setUp(self):
+        self.my_interpreter = ConcatInterpreter()
+        self.my_interpreter.interpret_file("../and.txt")
+        self.my_interpreter.interpret_file("../or.txt")
+
+    def test_and_true_true(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_and1.txt")
+        self.assertEqual(self.my_interpreter.stack, [True])
+
+    def test_and_true_false(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_and2.txt")
+        self.assertEqual(self.my_interpreter.stack, [False])
+
+    def test_and_false_true(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_and3.txt")
+        print(self.my_interpreter.stack)
+        self.assertEqual(self.my_interpreter.stack, [False])
+
+    def test_and_false_false(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_and4.txt")
+        print(self.my_interpreter.stack)
+        self.assertEqual(self.my_interpreter.stack, [False])
+
+    def test_or_true_true(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_or1.txt")
+        self.assertEqual(self.my_interpreter.stack, [True])
+
+    def test_or_true_false(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_or2.txt")
+        self.assertEqual(self.my_interpreter.stack, [True])
+
+    def test_or_false_true(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_or3.txt")
+        self.assertEqual(self.my_interpreter.stack, [True])
+
+    def test_or_false_false(self):
+        self.my_interpreter.interpret_file("test_input/boolean_input/test_or4.txt")
+        self.assertEqual(self.my_interpreter.stack, [False])
