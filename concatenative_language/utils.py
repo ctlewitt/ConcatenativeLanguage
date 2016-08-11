4974457cc972a05da36d9e0ba1e6f28a0b14902c
Charley Lewittes <ctlewitt@gmail.com>
2016-08-11 10:05:13 -0400

    cleaned up file; reorganized functions for clarity, improved comments, and removed vestigial code

--- concatenative_language/utils.py
+++ concatenative_language/utils.py
@@ -1,97 +1,5 @@
 import ast
 import fileinput
-from concatenative_language.constants import DEBUG_MODE
-
-
-
-def cast_to_number_if_possible(word):
-    try:
-        return int(word)
-    except ValueError:
-        return float(word)
-    # will raise exception if not int or float
-
-
-# checks against string "True" or "False" rather than casting to avoid accidentally casting other variables to boolean
-def cast_to_bool_if_possible(word):
-    if word == "True" or word is True:
-        return True
-    elif word == "False" or word is False:
-        return False
-    else:
-        raise ValueError("not a boolean")
-
-
-# # might need to account for case where function has saved string values without quotes
-# def cast_to_str_if_possible(word, origin):
-#     if (origin == "from file" and
-#             len(word) >= 2 and
-#             ((word[0] == "'" and word[-1] == "'") or (word[0] == '"' and word[-1] == '"'))):
-#         return word[1:len(word)-1]
-#     elif origin == "from function":
-#         return word
-#     else:
-#         raise ValueError("not a str")
-#     # else:
-#     #     return True, word
-
-
-# might need to account for case where function has saved string values without quotes
-def cast_to_str_if_possible(word):
-    if len(word) >= 2 and ((word[0] == "'" and word[-1] == "'") or (word[0] == '"' and word[-1] == '"')):
-        return word[1:len(word)-1]
-    raise ValueError("not a str")
-
-
-def cast_to_list_if_possible(word):
-    if word[0] == '[' and word[-1] == ']':
-        return ast.literal_eval(word)
-    raise ValueError("not a list")
-
-
-# checks if token is a function name and returns corresponding function
-# otherwise: tries to cast token string to a bool, int, float, list, or reformat to proper str and returns such
-def cast_to_val(token, interpreter):
-    casting_functions = [cast_to_bool_if_possible, cast_to_number_if_possible,
-                         cast_to_list_if_possible, cast_to_str_if_possible]
-    # token could be a function name
-    if interpreter.need_func_name:
-        # todo: should check to make sure that function is not a number, boolean, etc
-        if DEBUG_MODE:
-            print("just returning token in need_func_name situation: {}".format(token))
-        return token
-    if token in interpreter.functions:
-        return interpreter.functions[token]
-        # check if token to be cast to any variable type
-        #    kwargs = {token: token, origin: origin}
-    for function in casting_functions:
-        try:
-            return function(token)
-        except ValueError:
-            pass
-    raise TypeError("token {} is invalid variable type or function name; could not be resolved".format(token))
-
-
-# def append_with_type_cast(the_list, token, origin, functions=None):
-#     casting_functions = [cast_to_bool_if_possible, cast_to_number_if_possible,
-#                          cast_to_list_if_possible, cast_to_str_if_possible]
-#     # if token cannot be cast, then it could be a function name
-#     if functions and token in functions:
-#         the_list.append(token)
-#         return
-#     # check if token to be cast to any variable type
-# #    kwargs = {token: token, origin: origin}
-#     for function in casting_functions:
-#         try:
-#             cast_val = function(token, origin)
-#             the_list.append(cast_val)
-#             return
-#         except ValueError:
-#             pass
-#         # if successful_cast:
-#         #     the_list.append(cast_val)
-#         #     return
-#     raise TypeError("token {} is invalid variable type or function name; could not be resolved".format(token))
 
 
 # returns file-like object for interpreting
@@ -101,3 +9,66 @@ def get_input(source):
         return fileinput.input(files=source)
     else:
         return source
+
+
+# checks if token is a function name and returns corresponding function
+# otherwise: tries to convert token to a bool, int, float, list, or reformat to proper str
+# and returns such
+def cast_to_val(token, interpreter):
+    # token could be a function name for a function currently being defined
+    # e.g., func_name in "define func_name { body }"
+    if interpreter.need_func_name:
+        return token
+    # token corresponds with already-existing function; return function
+    if token in interpreter.functions:
+        return interpreter.functions[token]
+    # check if token could be a bool, int, float, list, or str and return "cast" value
+    casting_functions = [cast_to_bool_if_possible, cast_to_number_if_possible,
+                         cast_to_list_if_possible, cast_to_str_if_possible]
+    for function in casting_functions:
+        try:
+            return function(token)
+        except ValueError:
+            pass
+    # token was not a function name or valid variable; raise exception to notify programmer
+    raise TypeError("token {} is invalid variable type or function name; could not be resolved".format(token))
+
+
+# helper for cast_to_val:
+# casts a value to an int or a float if possible;
+# raises exception if neither int nor float
+def cast_to_number_if_possible(word):
+    try:
+        return int(word)
+    except ValueError:
+        return float(word)
+
+
+# helper for cast_to_val:
+# "casts" a value to True or False if appropriate; checks against string or value rather than aggressively casting
+# raises exception if not a boolean
+def cast_to_bool_if_possible(word):
+    if word == "True" or word is True:
+        return True
+    elif word == "False" or word is False:
+        return False
+    else:
+        raise ValueError("not a boolean")
+
+
+# helper for cast_to_val:
+# "casts" a value to string, removing leading and trailing quotation marks;
+# raises an exception if not a string
+def cast_to_str_if_possible(word):
+    if len(word) >= 2 and ((word[0] == "'" and word[-1] == "'") or (word[0] == '"' and word[-1] == '"')):
+        return word[1:len(word)-1]
+    raise ValueError("not a str")
+
+
+# helper for cast_to_val:
+# "casts" a value to list, using ast.literal_eval (cannot handle non-literals, unfortunately, so no lists of functions)
+# raises an exception if not a list
+def cast_to_list_if_possible(word):
+    if word[0] == '[' and word[-1] == ']':
+        return ast.literal_eval(word)
+    raise ValueError("not a list")
