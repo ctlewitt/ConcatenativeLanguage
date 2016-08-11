022dc5ba4ef8003515f1d0b4164f20b18395d372
Charley Lewittes <ctlewitt@gmail.com>
2016-08-11 09:46:14 -0400

    moved functions that print into output_functions

--- concatenative_language/functions/calculator_functions.py
+++ concatenative_language/functions/calculator_functions.py
@@ -1,9 +1,11 @@
-
+# num num -> num
+# takes two numbers and returns their sum
 def add(interpreter):
     interpreter.execute(interpreter.functions['swap'])
     interpreter.stack.append(interpreter.stack.pop() + interpreter.stack.pop())
 
-
+# num num -> num
+# takes two numbers and returns the different between the first and the second (deepest num first)
 def sub(interpreter):
     interpreter.execute(interpreter.functions['swap'])
     interpreter.stack.append(interpreter.stack.pop() - interpreter.stack.pop())
@@ -13,21 +15,24 @@ def mul(interpreter):
     interpreter.execute(interpreter.functions['swap'])
     interpreter.stack.append(interpreter.stack.pop() * interpreter.stack.pop())
 
-
+# num num -> int (could be rounded float if either num was a float)
+# takes two numbers and returns the first divided by the second (deepest num first)
+# uses integer division
 def int_div(interpreter):
     interpreter.execute(interpreter.functions['swap'])
     interpreter.stack.append(interpreter.stack.pop() // interpreter.stack.pop())
 
 
+# num num -> float
+# takes two numbers and returns the first divided by the second (deepest num first)
+# uses floating point division
 def float_div(interpreter):
     interpreter.execute(interpreter.functions['swap'])
     interpreter.stack.append(interpreter.stack.pop() / interpreter.stack.pop())
 
 
+# entire_stack -> []
+# removes everything from the stack
+# warning: very destructive
 def clr(interpreter):
-    interpreter.stack.pop()
-
-
-def prt(interpreter):
-    print(interpreter.stack[-1])
-
+    interpreter.stack = []
